93e1a0996835068d7ea8966e2428f869aa9541e9
cha-yh <chayh311@gmail.com>
2021-01-06 13:23:13 +0900

    feat: Add a scroll event for hiding button or nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import {Link, Element} from 'react-scroll';
 
 export default function Home() {
+    const [isLower, setIsLower] = useState(false);
+    const [prePageY, setPrePageY] = useState(0);
+    const [scrollMoving, setScrollMoving] = useState<"UP"|"DOWN">("UP");
+    const scrollHandler = () => {
+        const pageYOffset = window.pageYOffset;
+        console.log('current page y', window.pageYOffset);
+        console.log('pre page y', prePageY);
+
+        if(prePageY > pageYOffset) {
+            console.log('up')
+            setScrollMoving("UP");
+        } else {
+            console.log('down')
+            setScrollMoving("DOWN");
+        }
+        setPrePageY(pageYOffset);
+        setIsLower(window.pageYOffset > 60);
+        return;
+    }
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler);
+        return () => {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }, [prePageY])
     return (
         <div>
-            <header className="w-full fixed top-0 z-10 bg-white">
+            <header className={`w-full fixed top-0 z-10 bg-white`}>
                 <div className="flex justify-between items-center container mx-auto">
                     <Link activeClass="" to="section1" spy={true} smooth={true} offset={-60} duration={200}>
                         <img src="/apple.png" alt="" className="w-20"/>
@@ -11,23 +37,23 @@ export default function Home() {
                     
                     <ul className="flex">
                         <li className="mx-2">
-                            <Link activeClass="text-green-500" to="testid1" spy={true} smooth={true} offset={-70} duration={200}>
+                            <Link activeClass="text-green-500" to="testid1" spy={true} smooth={true} offset={-80} duration={200}>
                                 menu 1
                             </Link>
                         </li>
                         <li className="mx-2">
-                            <Link activeClass="text-green-500" to="testid2" spy={true} smooth={true} offset={-70} duration={200}>
+                            <Link activeClass="text-green-500" to="testid2" spy={true} smooth={true} offset={-80} duration={200}>
                                 menu 2
                             </Link>
                         </li>
                         <li className="mx-2">
-                            <Link activeClass="text-green-500" to="testid3" spy={true} smooth={true} offset={-70} duration={200}>
+                            <Link activeClass="text-green-500" to="testid3" spy={true} smooth={true} offset={-80} duration={200}>
                                 cards
                             </Link>
                         </li>
 
                         <li className="mx-2">
-                            <Link activeClass="text-green-500" to="contact-section" spy={true} smooth={true} offset={-70} duration={200}>
+                            <Link activeClass="text-green-500" to="contact-section" spy={true} smooth={true} offset={-80} duration={200}>
                                 contanct
                             </Link>
                         </li>
@@ -38,11 +64,19 @@ export default function Home() {
             {/* header dummy for space */}
             <div className="" style={{height: "60px"}}/>
 
+            {/* scroll top button */}
+            <button
+                className={`w-16 h-16 p-2 border border-gray-800 bg-gray-800 text-white z-20 fixed bottom-5 right-5 transition-all duration-500 ${!isLower ? "opacity-0" : "opacity-100"} `}
+                onClick={() => {window.scroll({top:0, left: 0, behavior: "smooth"})}}
+            >
+                TOP
+            </button>
+
             <div className="">
                 <Element id="section1" className="relative " style={{height: 'calc(100vh - 60px)'}}>
                     <div
                         className="w-full h-full bg-cover bg-center"
-                        style={{backgroundImage: 'url(https://cdn.pixabay.com/photo/2020/12/20/10/01/advent-5846564_960_720.jpg)'}}
+                        style={{backgroundImage: 'url(https://images.unsplash.com/photo-1609795386999-182f7609dc74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'}}
                     />
 
                     {/* dimmer */}
@@ -75,8 +109,8 @@ export default function Home() {
                             {imgUrl: "https://images.unsplash.com/photo-1609767500458-d2a133f61cab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", title: "rabit", contents: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, similique."},
                             {imgUrl: "https://images.unsplash.com/photo-1609770242416-21d590c2a157?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", title: "car", contents: "Lorem ipsum dolor sit, amet consectetur adipisicing."},
                             {imgUrl: "https://images.unsplash.com/photo-1609700731241-d4b42fa1f483?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", title: "snow", contents: "Lorem ipsum dolor sit, amet consectetur adipisicing elit."},
-                        ].map(item => (
-                            <div className="mx-2 flex-1">
+                        ].map((item, index) => (
+                            <div className="mx-2 flex-1" key={index}>
                                 <img src={item.imgUrl} alt="" className="w-60 h-96 object-cover"/>
                                 <h5>{item.title}</h5>
                                 <p>{item.contents}</p>
