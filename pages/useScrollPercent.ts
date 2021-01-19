1b1c9eff0866043129e5ad6b5b6c6d117aa4970c
cha-yh <chayh311@gmail.com>
2021-01-19 21:30:42 +0900

    chore: Remove console log

--- pages/useScrollPercent.ts
+++ pages/useScrollPercent.ts
@@ -6,35 +6,24 @@ export default function useScrollPercent(wrapperRef: any) {
 
     useEffect(() => {
         let observer;
-        console.log('wrapperRef', wrapperRef);
         if (wrapperRef) {
             observer = new IntersectionObserver(handleIntersection, { threshold: THRESHOLD });
             observer.observe(wrapperRef.current.stickyWrapper);
         }
 
         return () => {
-            console.log('returned')
             return observer && observer.disconnect()
         };
 
     }, [wrapperRef])
 
     const handleScrollInSection = () => {
-        const viewPortHeight = window.innerHeight;
         const sectionHeight = wrapperRef.current.sectionWrapper.getBoundingClientRect().height;
-        const sectionOffsetY = wrapperRef.current.sectionWrapper.offsetTop ;
-        console.log('sectionOffsetY', sectionOffsetY);
+        const sectionOffsetY = wrapperRef.current.sectionWrapper.offsetTop;
         const currentY = window.pageYOffset;
         const startY = sectionOffsetY - (sectionHeight * THRESHOLD);
-        // const endY = sectionOffsetY + sectionHeight + sectionHeight * (1 - THRESHOLD);
         const endY = startY + sectionHeight;
-        console.log('startY', startY)
-        console.log('endY', endY)
-        console.log('currentY', currentY)
-        const gap = endY - startY;
         const percent = Math.floor((currentY - startY + 1) / (endY - startY) * 100);
-
-        console.log('percent', percent);
         setPercent(percent);
 
     }
