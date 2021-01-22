d41909a807ae062ba04ce9a0e9a2ea923f1d5c64
cha-yh <chayh311@gmail.com>
2021-01-22 15:39:40 +0900

    feat: Update PicTextSection animation

--- pages/PicTextSection.tsx
+++ pages/PicTextSection.tsx
@@ -9,16 +9,17 @@ export default function PicTextSection() {
 
     const [imageSize, setImageSize] = useState(0.8);
     const [imgOpacity, setImgOpacity] = useState(0.3);
+    const [imgHorizontal, setImgHorizontal] = useState(0);
     const [textOpacity, setTextOpacity] = useState(0);
 
-    const getCalculatedValueByPercent = (start, end, percent, min = 0) => {
+    const getCalculatedValueByPercent = (start, end, percent, min = 0, max = 1) => {
         const diff = end - start;
-        const x = (1-min) / diff;
+        const x = (max-min) / diff;
         const calculatedValue = min + x * (percent - start);
         return calculatedValue < min
             ? min
-            : calculatedValue > 1
-                ? 1
+            : calculatedValue > max
+                ? max
                 : calculatedValue;
         
     }
@@ -26,7 +27,8 @@ export default function PicTextSection() {
     useEffect(() => {
         setTextOpacity(getCalculatedValueByPercent(20, 40, percent));
         setImgOpacity(getCalculatedValueByPercent(0, 30, percent, 0.3));
-        setImageSize(getCalculatedValueByPercent(0, 20, percent, 0.9));
+        setImageSize(getCalculatedValueByPercent(0, 20, percent, 0.8));
+        setImgHorizontal(getCalculatedValueByPercent(0, 30, percent, -100 , 0) * -1);
     }, [percent])
 
     return (
@@ -39,9 +41,9 @@ export default function PicTextSection() {
             <Element className="flex flex-col items-center justify-center py-40" id="testid1">
                 <img
                     src="https://images.unsplash.com/photo-1609729015759-8f18dd32f562?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1348&q=80" alt=""
-                    className="object-cover h-96 flex-1 mx-2"
+                    className="object-cover h-96 flex-1 mx-2 mb-10"
                     style={{
-                        transform: `matrix(${imageSize}, 0, 0, ${imageSize}, 0, ${-0.2 * percent})`,
+                        transform: `matrix(${imageSize}, 0, 0, ${imageSize}, 0, ${imgHorizontal})`,
                         opacity: imgOpacity
                     }}
                 />
