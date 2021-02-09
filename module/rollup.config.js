56433671bc98c93b84354522beb1bb9f46f25b61
cha-yh <chayh311@gmail.com>
2021-02-09 23:44:55 +0900

    Initiate npm package settings

---
+++ module/rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+process.env.BABEL_ENV = 'production';
+
+export default {
+  input: './src/index.ts', 
+  plugins: [
+    peerDepsExternal(),
+    resolve({ extensions }),
+    babel({ extensions, include: ['src/**/*'], runtimeHelpers: true }),
+  ],
+  output: [
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ]
+};
